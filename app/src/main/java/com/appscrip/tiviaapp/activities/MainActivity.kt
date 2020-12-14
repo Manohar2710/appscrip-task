16532b2a404a445ff35a0f3ea593e28b41351faf
Manohar2710 <manohar@futretailtech.in>
2020-12-14 18:29:43 +0530

    gradel update

--- app/src/main/java/com/appscrip/tiviaapp/activities/MainActivity.kt
+++ app/src/main/java/com/appscrip/tiviaapp/activities/MainActivity.kt
@@ -2,12 +2,20 @@ package com.appscrip.tiviaapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.appscrip.tiviaapp.R
 import com.appscrip.tiviaapp.adapters.SurveyDataAdapter
+import com.appscrip.tiviaapp.applications.SurveyDataApplication
+import com.appscrip.tiviaapp.viewmodels.HomeActivityViewModel
+import com.appscrip.tiviaapp.viewmodels.HomeViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+	private val newWordActivityRequestCode = 1
+	private val wordViewModel: HomeActivityViewModel by viewModels {
+		HomeViewModelFactory((application as SurveyDataApplication).repository)
+	}
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setTheme(R.style.mainAppTheme);
@@ -20,5 +28,14 @@ class MainActivity : AppCompatActivity() {
 		val adapter = SurveyDataAdapter()
 		recyclerView.adapter = adapter
 		recyclerView.layoutManager = LinearLayoutManager(this)
+
+		// Add an observer on the LiveData returned by getAlphabetizedWords.
+		// The onChanged() method fires when the observed data changes and the activity is
+		// in the foreground.
+		wordViewModel.allWords.observe(owner = this) { words ->
+			// Update the cached copy of the words in the adapter.
+			words.let { adapter.submitList(it) }
+		}
+
 	}
 }
